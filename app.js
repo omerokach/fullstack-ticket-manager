0806ad8366281ca540cfe46162837ad0c3ceb14d
omer rokach <77382841+omerokach@users.noreply.github.com>
2021-03-25 16:14:41 +0200

    update path to build

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const Ticket = require("./model/TicketSchema");
 const cors = require("cors");
 const app = express();
-
+const path = require("path");
 app.use(express.json());
-app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, "build")));
 app.use(cors());
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build"));
+});
 
 app.get("/api/tickets", (req, res) => {
   const { searchText } = req.query;
